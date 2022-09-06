86f887923c4607319a69238c2682f49efcde1bd1
QuanGiap <minhquangiapnguyen@gmail.com>
2022-09-05 21:12:39 -0700

    signup update

--- registration-backend/routes/UserRouter.js
+++ registration-backend/routes/UserRouter.js
@@ -2,6 +2,9 @@ const userRouter = require("express").Router();
 const bcrypt = require('bcrypt');
 const UserInform = require('../schemas/UserSchema')
 const SPECIAL_CHAR = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+const SPECIAL_CHAR_EMAIL_INVALID_PREFIX = /[`!@#$%^&*()+\=\[\]{};':"\\|,<>\/?~]/;
+const SPECIAL_CHAR_EMAIL_INVALID_DOMAIN = /[`!@#$%^&*()_+\=\[\]{};':"\\|,.<>\/?~]/;
+const SPECIAL_CHAR_EMAIL_VALID = '_-.';
 const COUNTRY_CODE =
   "AF AX AL DZ AS AD AO AI AQ AG AR AM AW AU AT AZ BH BS BD BB BY BE BZ BJ BM BT BO BQ BA BW BV BR IO BN BG BF BI KH CM CA CV KY CF TD CL CN CX CC CO KM CG CD CK CR CI HR CU CW CY CZ DK DJ DM DO EC EG SV GQ ER EE ET FK FO FJ FI FR GF PF TF GA GM GE DE GH GI GR GL GD GP GU  GT GG GN GW GY HT HM VA HN HK HU IS IN ID IR IQ IE IM IL IT JM JP JE JO KZ KE KI KP KR KW KG LA LV LB LS LR LY LI LT LU MO MK MG MW MY MV ML MT MH MQ MR MU YT MX FM MD MC MN ME MS MA MZ MM NA NR NP NL NC NZ NI NE NG NU NF MP NO OM PK PW PS PA PG PY PE PH PN PL PT PR QA RE RO RU RW BL SH KN LC MF PM VC WS SM ST SA SN RS SC SL SG SX SK SI SB SO ZA GS SS ES LK SD SR SJ SZ SE CH SY TW TJ TZ TH TL TG TK TO TT TN TR TM TC TV UG UA AE GB US UM UY UZ VU VE VN VG VI WF EH YE ZM ZW";
 
@@ -37,7 +40,7 @@ userRouter.post("/create", async function (req, res, next) {
     country:req.body.country,
     phoneNumber:req.body.phoneNumber || "",
   })
-  userInform.save().then(console.log("Save success")).catch(err=>console.log(err));
+  if(!req.body.isTest)userInform.save().then(console.log("Save success")).catch(err=>console.log(err));
   return sendRes("User successfully saved",res,201);
 });
 
@@ -50,6 +53,19 @@ function checkValid(body) {
   const errorPass = checkPassSecure(body.password);
   if (errorPass) return errorPass;
   if (COUNTRY_CODE.indexOf(body.country) == -1) return "Country code not valid";
+  const errorEmail = checkEmailValid(body.email);
+  if(errorEmail) return errorEmail;
+  return null;
+}
+
+function checkEmailValid(email){
+  const arrStr = email.split('@');
+  if(arrStr.length!=2) return "email invalid";
+  const prefix = arrStr[0];
+  const domain = arrStr[1];
+  if(prefix.length<2||domain.length<2) return "email is not long enough and/or is invalid";
+  if(!checkSpecialCharStr(prefix,1)) return "email invalid";
+  if(!checkSpecialCharStr(domain,2)) return "email invalid";
   return null;
 }
 
@@ -104,6 +120,28 @@ function sendRes(message, res, statusCode = 400) {
 
 function enoughStringLength(str, min = 1, max) {
   return str.length >= min && str.length <= max;
+}
+
+function checkSpecialCharStr(str,type){
+  // Allowed characters: letters (a-z), numbers, underscores, periods, and dashes.
+  if(type === 1 && SPECIAL_CHAR_EMAIL_INVALID_PREFIX.test(str)) return false;
+  if(type === 2){
+    const arrStr = str.split('.');
+    if(arrStr.length !== 2) return false;
+    if(arrStr[1].length<2) return false;
+    if(SPECIAL_CHAR_EMAIL_INVALID_DOMAIN.test(arrStr[0])) return false;
+    str = arrStr[0];
+  }
+  //  An underscore, period, or dash must be followed by one or more letter or number.
+  if(SPECIAL_CHAR_EMAIL_VALID.indexOf(str[0])!==-1||SPECIAL_CHAR_EMAIL_VALID.indexOf(str[str.length-1])!==-1) return false;
+  for(let i = 1;i<str.length;i++){
+    const character = str[i];
+    const beforeChar = str[i-1];
+    if(SPECIAL_CHAR_EMAIL_VALID.indexOf(character)!==-1 && SPECIAL_CHAR_EMAIL_VALID.indexOf(beforeChar)!==-1){
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = userRouter;
